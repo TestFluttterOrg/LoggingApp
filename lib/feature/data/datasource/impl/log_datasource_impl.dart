daf6c1a075757050b1ae2504b0dadde815069f62
Josef Guillen <josefguillen1996@gmail.com>
2024-11-22 15:53:35 +0800

    Created function for fetching and saving data from DB

--- lib/feature/data/datasource/impl/log_datasource_impl.dart
+++ lib/feature/data/datasource/impl/log_datasource_impl.dart
@@ -1,5 +1,7 @@
 import 'package:logging_app/feature/data/constants/db_constants.dart';
 import 'package:logging_app/feature/data/datasource/log_datasource.dart';
+import 'package:logging_app/feature/domain/entity/data/log/log_entity.dart';
+import 'package:logging_app/feature/domain/model/result_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class LogDataSourceImpl extends LogDataSource {
@@ -16,5 +18,51 @@ class LogDataSourceImpl extends LogDataSource {
         "${DBConstants.logColMessage}" TEXT NOT NULL,
         "${DBConstants.logColCreatedAt}" TEXT NOT NULL
         );""");
+  }
+
+  @override
+  Future<ResultModel<int>> saveLog(LogEntity data) async {
+    try {
+      final result = await database.rawInsert(
+        '''INSERT INTO (
+        ${DBConstants.logColName},
+        ${DBConstants.logColMessage},
+        ${DBConstants.logColCreatedAt}
+        ) VALUES (?, ?, ?)''',
+        [
+          data.name,
+          data.message,
+          data.createdAt,
+        ],
+      );
+      return ResultModel(
+        isSuccess: true,
+        message: "Log saved successfully",
+        data: result,
+      );
+    } catch (_) {
+      return const ResultModel(
+        isSuccess: false,
+        message: "Log save failed",
+      );
+    }
+  }
+
+  @override
+  Future<ResultModel<List<LogEntity>>> getLogs() async {
+    try {
+      final result = await database.rawQuery(
+          "SELECT * FROM ${DBConstants.logColTblName} ORDER BY ${DBConstants.logColId} DESC");
+      final data = result.map((e) => LogEntity.fromJson(e)).toList();
+      return ResultModel(
+        isSuccess: true,
+        data: data,
+      );
+    } catch (_) {
+      return const ResultModel(
+        isSuccess: false,
+        message: "Failed to get logs",
+      );
+    }
   }
 }
--- lib/feature/data/datasource/log_datasource.dart
+++ lib/feature/data/datasource/log_datasource.dart
@@ -1,3 +1,7 @@
-abstract class LogDataSource {
+import 'package:logging_app/feature/domain/entity/data/log/log_entity.dart';
+import 'package:logging_app/feature/domain/model/result_model.dart';
 
+abstract class LogDataSource {
+  Future<ResultModel<int>> saveLog(LogEntity data);
+  Future<ResultModel<List<LogEntity>>> getLogs();
 }
